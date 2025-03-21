35640f657d7d964e1a387f2280ce59d1452202a1
tony1224 <starmine2013@gmail.com>
2025-03-22 02:48:33 +0900

    refactoring

--- SwiftUIWorkshop/Package.swift
+++ SwiftUIWorkshop/Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUIWorkshopPackage",
+    name: "SwiftUIWorkshop",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftUIWorkshopPackage",
-            targets: ["SwiftUIWorkshopPackage"]),
+            name: "SwiftUIWorkshop",
+            targets: ["SwiftUIWorkshop"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SwiftUIWorkshopPackage", path: "Sources"),
+        .target(name: "SwiftUIWorkshop", path: "Sources"),
 
     ]
 )
